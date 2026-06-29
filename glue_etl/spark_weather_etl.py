72c65b9bad002517e42576308d28798da1210d9c
Marek Marcinko <161241232+MasnyMaras@users.noreply.github.com>
2026-06-29 12:23:04 +0200

    Add ETL script for processing weather data
    This script reads raw weather data from S3, validates it, and saves the cleaned data in Parquet format.

---
+++ glue_etl/spark_weather_etl.py
@@ -0,0 +1,35 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Sciezki do Twoich magazynow S3
+raw_path = "s3://weather-raw-data-296066093533-us-east-1-an/"
+curated_path = "s3://weather-curated-data-296066093533-us-east-1-an/"
+
+# 1. Wczytanie surowych danych
+df = spark.read.json(raw_path)
+
+# 2. Walidacja i wyciagnięcie konkretnych kolumn z pliku
+df_cleaned = df.selectExpr(
+    "name as city",
+    "main.temp as temp_kelvin",
+    "main.humidity as humidity",
+    "weather[0].main as weather_condition",
+    "dt as timestamp_unix"
+)
+
+# 3. Zapis przetworzonych danych
+df_cleaned.write.mode("append").parquet(curated_path)
+
+job.commit()
